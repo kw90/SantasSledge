39f70453326409e4f60afba13f2502ef39d41dc5
Riedi Linus <linriedi@hotmail.com>
2016-12-15 14:33:19 +0100

    .

---
+++ Santa/Common/Alogs/GiftExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Alogs
+{
+    public static class GiftExtensions
+    {
+        public static double DistanceTo(this Gift one, Gift two)
+        {
+            var phyOne = one.Latitude / 360 * Math.PI * 2;
+            //var phyTwo 
+
+            return 0;
+        }
+
+        private static double ToRadiant(double degrees)
+        {
+            return degrees / 360 * Math.PI * 2;
+        }
+    }
+}
