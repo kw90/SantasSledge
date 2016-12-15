6a50a127c0eaf484ba58b6a46f20f445da6f3d42
Fux Etienne tcfux <etienne.fux@hslu.ch>
2016-12-15 13:36:04 +0100

    Add tests for tour validity

--- Santa/Giftgrouping/CsvIO/Reader.cs
+++ Santa/Giftgrouping/CsvIO/Reader.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using Common;
 
 namespace Giftgrouping.CsvIO
 {
---
+++ Santa/Tests/Common/TourTest.cs
@@ -0,0 +1,32 @@
+﻿using Common;
+using NUnit.Framework;
+
+namespace Tests.Common
+{
+    [TestFixture]
+    public class TourTest
+    {
+        private Tour testee;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            testee = new Tour();
+            testee.AddGift(new Gift(1, 500, 0, 0));
+            testee.AddGift(new Gift(2, 500, 4, 2));
+        }
+
+        [Test]
+        public void TestValidTourWeight()
+        {
+            Assert.IsTrue(testee.IsValid());
+        }
+
+        [Test]
+        public void TestInvalidTourWeight()
+        {
+            testee.AddGift(new Gift(3, 0.001, 5, 5));
+            Assert.IsFalse(testee.IsValid());
+        }
+    }
+}
