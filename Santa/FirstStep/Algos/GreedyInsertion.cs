7abd1e5b31e53dd1d9d06b30f211c007008d67f8
Riedi Linus <linriedi@hotmail.com>
2016-12-15 18:59:51 +0100

    Clean ups

--- Santa/FirstStep/Algos/GreedyInsertion.cs
+++ Santa/FirstStep/Algos/GreedyInsertion.cs
@@ -1,6 +1,5 @@
 ﻿using Common;
 using Common.Alogs;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -79,63 +78,6 @@ namespace FirstStep.Algos
             }
 
             return tour;
-
-            ////Array with the indices of the next nodes
-            //int[] nextIndices = new int[points.Count()];
-
-            ////Initial partial tour 0 -> 1 -> 0
-            //nextIndices[0] = 1;
-            //double currentWeight = 0;
-
-            //if(points.Count == 2)
-            //{
-            //    if(maxWeight < points.Select(g => g.Weight).Sum())
-            //    {
-            //        return new List<Gift>();
-            //    }
-            //}
-
-            ////Find the best position to insert for each remaining point
-            //for (int i = 2; i < points.Count(); i++)
-            //{
-            //    double lowestDistanceIncrease = Double.PositiveInfinity;
-            //    int lowestDistanceIncreaseIdx = -1;
-
-            //    for (int j = 0; j < i; j++)
-            //    {
-            //        //Increased cost of tour if point i is inserted in place j
-            //        double distanceIncrease =
-            //            points[j].DistanceTo(points[i])
-            //            + points[i].DistanceTo(points[nextIndices[j]])
-            //            - points[j].DistanceTo(points[nextIndices[j]]);
-
-            //        if (distanceIncrease < lowestDistanceIncrease)
-            //        {
-            //            lowestDistanceIncrease = distanceIncrease;
-            //            lowestDistanceIncreaseIdx = j;
-            //        }
-            //    }
-
-            //    currentWeight += nextIndices[lowestDistanceIncreaseIdx];
-            //    if(currentWeight > maxWeight)
-            //    {
-            //        break;
-            //    }
-
-            //    nextIndices[i] = nextIndices[lowestDistanceIncreaseIdx];
-            //    nextIndices[lowestDistanceIncreaseIdx] = i;
-            //}
-
-            ////Walk along next indices to build solution.
-            //List<Gift> solution = new List<Gift>();
-            //int index = 0;
-            //for (int i = 0; i < points.Count(); i++)
-            //{
-            //    solution.Add(points[index]);
-            //    index = nextIndices[index];
-            //}
-
-            //return solution;
         }
 
         private double CalculateLenghtIncrease(NorthPole northPole, List<Gift> tour, int insertionIndex, Gift giftToInsert)
