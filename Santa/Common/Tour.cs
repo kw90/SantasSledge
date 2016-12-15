3b289e79e0b73664590264df2207f39552255914
Fux Etienne tcfux <etienne.fux@hslu.ch>
2016-12-15 13:14:57 +0100

    Add Tour to common project

---
+++ Santa/Common/Tour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Tour
+    {
+        private List<Gift> Gifts;
+
+        public List<Gift> GetTour()
+        {
+            return Gifts;
+        }
+
+        public void AddGift(Gift gift)
+        {
+            Gifts.Add(gift);
+        }
+    }
+}
