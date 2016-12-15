135c9cb9e473a36f929113fa9806bcfe2bc5da73
Riedi Linus <linriedi@hotmail.com>
2016-12-15 15:20:38 +0100

    Create algo to calculate distance between gift points

--- Santa/Common/Alogs/GiftExtensions.cs
+++ Santa/Common/Alogs/GiftExtensions.cs
@@ -10,10 +10,31 @@ namespace Common.Alogs
     {
         public static double DistanceTo(this Gift one, Gift two)
         {
-            var phyOne = one.Latitude / 360 * Math.PI * 2;
-            //var phyTwo 
+            const double radius = 6371000;
 
-            return 0;
+            var phyOne = ToRadiant(one.Latitude);
+            var phyTwo = ToRadiant(two.Latitude);
+
+            var lambdaOne = ToRadiant(one.Longitude);
+            var lambdaTwo = ToRadiant(two.Longitude);
+
+            var underRoot =
+                PowTwo(Math.Sin((phyTwo - phyOne)/2))
+                + Math.Cos(phyOne)
+                * Math.Cos(phyTwo)
+                * PowTwo(Math.Sin((lambdaTwo - lambdaOne)/2));
+
+            var arg = Math.Sqrt(underRoot);
+
+            return 
+                2 
+                * radius
+                * Math.Asin(arg);
+        }
+
+        private static double PowTwo(double input)
+        {
+            return Math.Pow(input, 2);
         }
 
         private static double ToRadiant(double degrees)
