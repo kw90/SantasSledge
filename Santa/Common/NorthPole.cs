059f0c96b86cd9607271ce12b464ed27c7e2f155
Riedi Linus <linriedi@hotmail.com>
2016-12-15 16:34:37 +0100

    Add NorthPole

---
+++ Santa/Common/NorthPole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class NorthPole
+    {
+        public Location Location { get; private set; }
+
+        public NorthPole()
+        {
+            this.Location = new Location(90, 0);
+        }
+    }
+}
